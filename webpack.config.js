84e092aa01ef7ba5b551e06ad3bf2d88bbb780f3
viher3 <viher_3@hotmail.com>
2018-02-17 23:29:54 +0100

    webpack file with css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var Encore = require('@symfony/webpack-encore');
 
-var jsFiles = [
+var files = [
 
+	// JS
 	//'./assets/js/jquery.3.2.1.min.js',
 	'./assets/js/jquery.nice-select.js',
 	'./assets/js/bootstrap.min.js',
@@ -11,7 +12,17 @@ var jsFiles = [
 	'./assets/js/niceselect-activate.js',
 	'./assets/js/owl.carousel.min.js',
 	'./assets/js/popper.min.js',
-	'./assets/js/prefixfree.min.js'
+	'./assets/js/prefixfree.min.js',
+
+	// CSS
+	'./assets/css/animate.css',
+	'./assets/css/bootstrap.min.css',
+	'./assets/css/font-awesome.min.css',
+	'./assets/css/modal-video.min.css',
+	'./assets/css/nice-select.css',
+	'./assets/css/normalize.css',
+	'./assets/css/owl.carousel.min.css',
+	'./assets/css/responsive.css',
 
 ];
 
@@ -24,7 +35,7 @@ Encore
     .setPublicPath('/build')    
 
     // will create public/build/app.js and public/build/app.css
-    .addEntry('app', jsFiles)
+    .addEntry('app', files)
 
     // allow legacy applications to use $/jQuery as a global variable
     .autoProvidejQuery()
