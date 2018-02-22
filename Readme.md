ba16e835d20c17ad93ad4755c6ccdbe4b2573cd4
viher3 <viher_3@hotmail.com>
2018-02-22 22:58:05 +0100

    fos_js_routes updated

--- Readme.md
+++ Readme.md
@@ -1,4 +1,9 @@
+
 Reaccion Estudio Website
 ====================
 
 Currently on development ...
+
+### Generate the 'public/js/fos_js_routes.json' file:
+
+	bin/console fos:js-routing:dump --format=json --target=public/js/fos_js_routes.json
---
+++ web/js/fos_js_routes.js
@@ -0,0 +1,1 @@
+fos.Router.setData({"base_url":"","routes":{"sendContactEmail":{"tokens":[["text","\/send-contact-email"]],"defaults":[],"requirements":[],"hosttokens":[],"methods":["POST"],"schemes":[]},"wePhoneYouEmail":{"tokens":[["text","\/we-phone-you-email"]],"defaults":[],"requirements":[],"hosttokens":[],"methods":["POST"],"schemes":[]}},"prefix":"","host":"localhost","scheme":"http"});
